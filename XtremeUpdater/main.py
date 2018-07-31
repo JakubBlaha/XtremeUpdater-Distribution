115f4bb55634b4d1a526582ad119ed7e7bb7a563
JakubBlaha <jakub.blaha@gmail.com>
2018-07-31 11:44:53 +0200

    v0.5.14.10
    Auto-update test.

--- XtremeUpdater/main.py
+++ XtremeUpdater/main.py
@@ -875,7 +875,7 @@ class XtremeUpdaterApp(App):
         self.root.goto_page(4)
 
 
-__version__ = '0.5.14.9'
+__version__ = '0.5.14.10'
 
 if __name__ == '__main__':
     xtremeupdater = XtremeUpdaterApp()
